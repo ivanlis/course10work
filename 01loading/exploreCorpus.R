ee6eeb755d7ca72dadd4f47688e36d5e273032b0
ivanlis <ivanlis.bilbao@gmail.com>
2018-08-11 16:52:44 +0200

    Add some more actions on tokens

--- 01loading/exploreCorpus.R
+++ 01loading/exploreCorpus.R
@@ -11,6 +11,8 @@ library(readtext)
 
 #enCorpus <- corpus_reshape(enCorpus, to = "sentences", use_docvars = FALSE)
 
+tokensDir <- "results"
+
 # Create many corpora, analyze them and remove
 files <- list.files(path = "../materials/datasets/english_split", 
                     pattern = "*.txt", 
@@ -19,7 +21,8 @@ files <- list.files(path = "../materials/datasets/english_split",
 
 cnt <- 0
 
-for (f in files)
+#for (f in files)
+for (f in files[1])
 {
     cat("Creating corpus", cnt, "from", f, "...\n")
     partCorpus <- corpus(readtext(f, cache = FALSE, verbosity = 3))
@@ -27,6 +30,24 @@ for (f in files)
     cat("Reshaping corpus", cnt, "...\n")
     partCorpus <- corpus_reshape(partCorpus, to = "sentences", use_docvars = FALSE)
     cat("Corpus", cnt, "reshaped.\n")
+    
+    # Now, tokenize this corpus
+    cat("Tokenizing corpus", cnt, "...\n")
+    toks <- tokens_tolower(tokens(partCorpus, what = "word", 
+                     remove_numbers = TRUE, 
+                     remove_punct = TRUE,
+                     remove_twitter = TRUE,
+                     remove_url = TRUE,
+                     include_docvars = FALSE
+                     ))
+    cat("Corpus", cnt, "tokenized.\n")
+    
+    tokFileName <- sprintf("%s/tokens_%02d.dat", tokensDir, cnt)
+    tokFile <- file(tokFileName, "w")
+    cat("Saving tokens to file ", tokFileName, "\n")
+    serialize(toks, tokFile)
+    close(tokFile)
+    cat("Tokens for corpus", cnt, "saved.\n")
     
     cnt <- cnt + 1
 }
